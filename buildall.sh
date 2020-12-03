e7679a5f1971920f4abdd812ad7775f9bbf3cd8d
Adam (ano95) <adam@ano95.eu>
2020-12-03 13:30:26 +0100

    Autodetect data source

--- buildall.sh
+++ buildall.sh
@@ -4,6 +4,6 @@ for school in zs ss any; do
   mkdir -p "build/$school"
 
   for science in mat fyz inf other any; do
-    python build.py --science $science --school $school -o "build/$school/$science.ics" --data-source http://data.kockatykalendar.sk/2020_21.json
+    python build.py --science $science --school $school -o "build/$school/$science.ics"
   done
 done
