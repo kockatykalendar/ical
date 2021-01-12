26ab7d0c48020a7d41ee3fcabadbf769624ee25b
Adam (ano95) <adam@ano95.eu>
2021-01-12 13:57:57 +0100

    Update to latest schema, use our Python wrapper

--- build.py
+++ build.py
@@ -1,9 +1,12 @@
 import argparse
 import icalendar
 import sys
+import kockatykalendar.api as kkapi
 from datetime import date, datetime
 import requests
 import json
+
+from kockatykalendar.events import Event, EventContestant, EventType, EventScience
 from pytz import timezone
 
 
@@ -13,14 +16,14 @@ PREFIX_ERROR = "\u001b[31m✗\u001b[0m"
 TZ = timezone("Europe/Bratislava")
 LANG = {    # TODO: Sync with frontend
     "types": {
-        "sutaz": "súťaž",
-        "seminar": "seminár",
-        "sustredenie": "sústredenie",
-        "vikendovka": "víkendovka",
-        "tabor": "tábor",
-        "olympiada": "olympiáda",
-        "prednasky": "prednášky",
-        "other": "iný druh akcie",
+        EventType.SUTAZ: "súťaž",
+        EventType.SEMINAR: "seminár",
+        EventType.SUSTREDENIE: "sústredenie",
+        EventType.VIKENDOVKA: "víkendovka",
+        EventType.TABOR: "tábor",
+        EventType.OLYMPIADA: "olympiáda",
+        EventType.PREDNASKY: "prednášky",
+        EventType.OTHER: "iný druh akcie",
     },
     "organizers": {
         "trojsten": "Trojsten",
@@ -33,19 +36,19 @@ LANG = {    # TODO: Sync with frontend
         "matfyz": "FMFI UK",
     },
     "contestant_types": {
-        "zs": "ZŠ",
-        "ss": "SŠ",
+        EventContestant.SchoolType.ZAKLADNA: "ZŠ",
+        EventContestant.SchoolType.STREDNA: "SŠ",
     },
     "sciences": {
-        "mat": "MAT",
-        "fyz": "FYZ",
-        "inf": "INF",
-        "other": "iné",
+        EventScience.MAT: "MAT",
+        EventScience.FYZ: "FYZ",
+        EventScience.INF: "INF",
+        EventScience.OTHER: "iné",
     }
 }
 
 
-parser = argparse.ArgumentParser(description="KockatýKalendár.sk iCal builder v0.1")
+parser = argparse.ArgumentParser(description="KockatýKalendár.sk iCal builder v0.1.1")
 parser.add_argument("-d", "--data-source", help="Source JSON file to use while building. Defaults to current school year.")
 parser.add_argument("--school", help="School type to filter output. Defaults to any.", default="any", choices=["any", "zs", "ss"])
 parser.add_argument("--science", help="Science to filter output. Defaults to any.", default=["any"], choices=["any", "mat", "fyz", "inf", "other"], nargs="*")
@@ -55,38 +58,20 @@ args = parser.parse_args()
 
 # Select data source JSON file.
 if args.data_source:
-    data_source = args.data_source
+    dataset = args.data_source
+    print(PREFIX_OK, f"Using '{dataset}' as data source.", file=sys.stderr)
 else:
-    today = date.today()
-    current_school_year = today.year if today.month >= 9 else today.year - 1
-
-    # We will load index.json and find the correct filename for current school year.
-    index_data = requests.get("https://data.kockatykalendar.sk/index.json")
-    if index_data.status_code != 200:
-        print(PREFIX_ERROR, f"Could not load index.json file. HTTP {index_data.status_code}", file=sys.stderr)
-        raise SystemExit(1)
-
-    index_years = json.loads(index_data.text)
-    selected_file = None
-    for year in index_years:
-        if year["start_year"] == current_school_year:
-            selected_file = year["filename"]
-            break
-
-    if not selected_file:
-        print(PREFIX_ERROR, f"Could not find any data file for school year {current_school_year}.", file=sys.stderr)
-        raise SystemExit(1)
-
-    data_source = f"https://data.kockatykalendar.sk/{selected_file}"
-print(PREFIX_OK, f"Using '{data_source}' as data source.", file=sys.stderr)
+    dataset = kkapi.get_current_dataset()
+    print(PREFIX_OK, f"Using school year {dataset.school_year} as data source.", file=sys.stderr)
 
 
 # Load JSON data.
-calendar_res = requests.get(data_source)
-if calendar_res.status_code != 200:
-    print(PREFIX_ERROR, f"Could not read data. HTTP {calendar_res.status_code}", file=sys.stderr)
-calendar_data = json.loads(calendar_res.text)
+events = kkapi.get_events(dataset)
 print(PREFIX_WORK, "Generating iCal file...", file=sys.stderr)
+
+
+def format_contestant(contestant: EventContestant):
+    return "%s %d" % (LANG["contestant_types"][contestant.type], contestant.year)
 
 
 # School filter:
@@ -112,7 +97,7 @@ def school_filter(event):
     return args.school in event_schools
 
 
-filtered_events = filter(school_filter, calendar_data)
+filtered_events = filter(school_filter, events)
 filtered_events = filter(science_filter, filtered_events)
 
 ical = icalendar.Calendar()
@@ -120,38 +105,36 @@ ical.add("prodid", "-//KockatyKalendar.sk//v0.1//")
 ical.add("version", "2.0")  # ical version
 
 for event in filtered_events:
+    event: Event = event    # TODO: Remove this line.
     ical_event = icalendar.Event()
-    ical_event.add("summary", ("(Zrušený)" if "cancelled" in event and event["cancelled"] else "") + event["name"])
+    ical_event.add("summary", ("(Zrušený)" if event.cancelled else "") + event.name)
+    ical_event.add("dtstart", TZ.localize(datetime.combine(event.date.start, datetime.min.time())).date())
 
-    event_start = datetime.strptime(event["date"]["start"], "%Y-%m-%d")
-    event_start = TZ.localize(event_start)
-    ical_event.add("dtstart", event_start.date())
+    if event.date.end:
+        ical_event.add("dtend", TZ.localize(datetime.combine(event.date.end, datetime.max.time())).date())
 
-    if "end" in event["date"]:
-        event_end = datetime.strptime(event["date"]["end"], "%Y-%m-%d")
-        event_end = TZ.localize(event_end)
-        ical_event.add("dtend", event_end.date())
+    if event.contestants.min.type is None and event.contestants.max.type is None:
+        contestants = "ktokoľvek"
+    elif event.contestants.min.type is None:
+        contestants = format_contestant(event.contestants.max) + " a mladší"
+    elif event.contestants.max.type is None:
+        contestants = format_contestant(event.contestants.min) + " a starší"
+    else:
+        contestants = format_contestant(event.contestants.min) + " – " + format_contestant(event.contestants.max)
 
-    contestants = LANG["contestant_types"][event["contestants"]["min"][0:2]] + " " + event["contestants"]["min"][2:]
-    if "max" in event["contestants"]:
-        contestants += " - "
-        if event["contestants"]["min"][0:2] != event["contestants"]["max"][0:2]:
-            contestants += LANG["contestant_types"][event["contestants"]["max"][0:2]] + " "
-        contestants += event["contestants"]["max"][2:]
-
-    type = LANG["types"][event["type"]]
-    sciences = ", ".join(map(lambda x: LANG["sciences"][x], event["sciences"]))
+    type = LANG["types"][event.type]
+    sciences = ", ".join(map(lambda x: LANG["sciences"][x], event.sciences))
     description = f"{type} | {sciences} | {contestants}\n"
 
-    if "info" in event:
-        description += f"\n{event['info']}\n"
+    if event.info:
+        description += f"\n{event.info}\n"
 
-    organizers = ", ".join(map(lambda x: LANG["organizers"][x] if x in LANG["organizers"] else x, event["organizers"]))
+    organizers = ", ".join(map(lambda x: LANG["organizers"][x] if x in LANG["organizers"] else x, event.organizers))
     description += f"\nOrganizátor(i): {organizers}"
 
-    if "link" in event:
-        ical_event.add("url", event["link"])
-        description += f"\n{event['link']}"
+    if event.link:
+        ical_event.add("url", event.link)
+        description += f"\n{event.link}"
 
     ical_event.add("description", description)
     ical.add_component(ical_event)
